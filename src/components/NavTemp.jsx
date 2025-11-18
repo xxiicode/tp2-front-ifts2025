589eaed4d07c6c2cea1468632d940fb34d64fb51
xxiicode <leandrobilokapic@gmail.com>
2025-11-18 16:23:17 +0100

    volver atras

--- src/components/NavTemp.jsx
+++
@@ -1,3 +0,0 @@
-const Nav = () => <h2>Soy un NAV!!</h2>
-
-export default Nav
--- src/components/SoundBtn.jsx
+++
@@ -1,8 +0,0 @@
-import useSound from 'use-sound'
-
-import boopSfx from '../assets/sounds/smb_coin.wav'
-
-export const SoundBtn = () => {
-  const [play] = useSound(boopSfx)
-  return <button onClick={play}>Boop!</button>
-}
